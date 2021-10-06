74dd09d58696f9aa471a3bee14d110638bff8d67
Panos Economou <panos.economou.v@gmail.com>
2021-10-06 19:14:15 +0400

    Lab computer

--- V2/build/CMakeFiles/haloscopeSim.dir/DependInfo.cmake
+++ V2/build/CMakeFiles/haloscopeSim.dir/DependInfo.cmake
@@ -4,27 +4,32 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/panos/Documents/NYU/6.Extracurricular/11.Haloscope/0.Simulations/V2/V2.cc" "/Users/panos/Documents/NYU/6.Extracurricular/11.Haloscope/0.Simulations/V2/build/CMakeFiles/haloscopeSim.dir/V2.cc.o"
-  "/Users/panos/Documents/NYU/6.Extracurricular/11.Haloscope/0.Simulations/V2/src/ActionInitialization.cc" "/Users/panos/Documents/NYU/6.Extracurricular/11.Haloscope/0.Simulations/V2/build/CMakeFiles/haloscopeSim.dir/src/ActionInitialization.cc.o"
-  "/Users/panos/Documents/NYU/6.Extracurricular/11.Haloscope/0.Simulations/V2/src/DetectorConstruction.cc" "/Users/panos/Documents/NYU/6.Extracurricular/11.Haloscope/0.Simulations/V2/build/CMakeFiles/haloscopeSim.dir/src/DetectorConstruction.cc.o"
-  "/Users/panos/Documents/NYU/6.Extracurricular/11.Haloscope/0.Simulations/V2/src/EventAction.cc" "/Users/panos/Documents/NYU/6.Extracurricular/11.Haloscope/0.Simulations/V2/build/CMakeFiles/haloscopeSim.dir/src/EventAction.cc.o"
-  "/Users/panos/Documents/NYU/6.Extracurricular/11.Haloscope/0.Simulations/V2/src/Hit.cc" "/Users/panos/Documents/NYU/6.Extracurricular/11.Haloscope/0.Simulations/V2/build/CMakeFiles/haloscopeSim.dir/src/Hit.cc.o"
-  "/Users/panos/Documents/NYU/6.Extracurricular/11.Haloscope/0.Simulations/V2/src/PrimaryGeneratorAction.cc" "/Users/panos/Documents/NYU/6.Extracurricular/11.Haloscope/0.Simulations/V2/build/CMakeFiles/haloscopeSim.dir/src/PrimaryGeneratorAction.cc.o"
-  "/Users/panos/Documents/NYU/6.Extracurricular/11.Haloscope/0.Simulations/V2/src/SD.cc" "/Users/panos/Documents/NYU/6.Extracurricular/11.Haloscope/0.Simulations/V2/build/CMakeFiles/haloscopeSim.dir/src/SD.cc.o"
+  "/home/po524/Documents/haloscope/V2/V2.cc" "/home/po524/Documents/haloscope/V2/build/CMakeFiles/haloscopeSim.dir/V2.cc.o"
+  "/home/po524/Documents/haloscope/V2/src/ActionInitialization.cc" "/home/po524/Documents/haloscope/V2/build/CMakeFiles/haloscopeSim.dir/src/ActionInitialization.cc.o"
+  "/home/po524/Documents/haloscope/V2/src/DetectorConstruction.cc" "/home/po524/Documents/haloscope/V2/build/CMakeFiles/haloscopeSim.dir/src/DetectorConstruction.cc.o"
+  "/home/po524/Documents/haloscope/V2/src/EventAction.cc" "/home/po524/Documents/haloscope/V2/build/CMakeFiles/haloscopeSim.dir/src/EventAction.cc.o"
+  "/home/po524/Documents/haloscope/V2/src/Hit.cc" "/home/po524/Documents/haloscope/V2/build/CMakeFiles/haloscopeSim.dir/src/Hit.cc.o"
+  "/home/po524/Documents/haloscope/V2/src/PrimaryGeneratorAction.cc" "/home/po524/Documents/haloscope/V2/build/CMakeFiles/haloscopeSim.dir/src/PrimaryGeneratorAction.cc.o"
+  "/home/po524/Documents/haloscope/V2/src/SD.cc" "/home/po524/Documents/haloscope/V2/build/CMakeFiles/haloscopeSim.dir/src/SD.cc.o"
   )
-set(CMAKE_CXX_COMPILER_ID "Clang")
+set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # Preprocessor definitions for this target.
 set(CMAKE_TARGET_DEFINITIONS_CXX
   "G4INTY_USE_QT"
   "G4INTY_USE_XT"
-  "G4LIB_BUILD_DLL"
+  "G4UI_USE"
   "G4UI_USE_QT"
   "G4UI_USE_TCSH"
+  "G4UI_USE_XM"
+  "G4VERBOSE"
+  "G4VIS_USE"
   "G4VIS_USE_OPENGL"
   "G4VIS_USE_OPENGLQT"
   "G4VIS_USE_OPENGLX"
+  "G4VIS_USE_OPENGLXM"
   "G4VIS_USE_RAYTRACERX"
+  "G4_STORE_TRAJECTORY"
   "QT_CORE_LIB"
   "QT_GUI_LIB"
   "QT_NO_DEBUG"
@@ -35,23 +40,15 @@ set(CMAKE_TARGET_DEFINITIONS_CXX
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/opt/geant4/geant4-10.4/include/Geant4"
   "../include"
-  "/Users/panos/geant4.10.06-install/include/Geant4"
-  "/usr/X11R6/include"
-  "/Library/Developer/CommandLineTools/SDKs/MacOSX10.15.sdk/System/Library/Frameworks/OpenGL.framework"
-  "/usr/local/Cellar/qt/5.15.0/lib/QtGui.framework"
-  "/usr/local/Cellar/qt/5.15.0/lib/QtGui.framework/Headers"
-  "/Library/Developer/CommandLineTools/SDKs/MacOSX10.15.sdk/System/Library/Frameworks/OpenGL.framework/Headers"
-  "/usr/local/Cellar/qt/5.15.0/lib/QtCore.framework"
-  "/usr/local/Cellar/qt/5.15.0/lib/QtCore.framework/Headers"
-  "/usr/local/Cellar/qt/5.15.0/./mkspecs/macx-clang"
-  "/usr/local/Cellar/qt/5.15.0/lib/QtWidgets.framework"
-  "/usr/local/Cellar/qt/5.15.0/lib/QtWidgets.framework/Headers"
-  "/usr/local/Cellar/qt/5.15.0/lib/QtPrintSupport.framework"
-  "/usr/local/Cellar/qt/5.15.0/lib/QtPrintSupport.framework/Headers"
-  "/usr/local/Cellar/qt/5.15.0/lib/QtOpenGL.framework"
-  "/usr/local/Cellar/qt/5.15.0/lib/QtOpenGL.framework/Headers"
-  "/usr/local/include"
+  "/opt/anaconda3/include/qt"
+  "/opt/anaconda3/include/qt/QtWidgets"
+  "/opt/anaconda3/include/qt/QtGui"
+  "/opt/anaconda3/include/qt/QtCore"
+  "/opt/anaconda3/./mkspecs/linux-g++"
+  "/opt/anaconda3/include/qt/QtPrintSupport"
+  "/opt/anaconda3/include/qt/QtOpenGL"
   )
 
 # Targets to which this target links.
--- V2/build/cmake_install.cmake
+++ V2/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/panos/Documents/NYU/6.Extracurricular/11.Haloscope/0.Simulations/V2
+# Install script for directory: /home/po524/Documents/haloscope/V2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -27,20 +27,32 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
+endif()
+
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
 if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "/Users/panos/Documents/NYU/6.Extracurricular/11.Haloscope/0.Simulations/V2/build/haloscopeSim")
   if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/haloscopeSim" AND
      NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/haloscopeSim")
-    execute_process(COMMAND /usr/bin/install_name_tool
-      -delete_rpath "/Users/panos/geant4.10.06-install/lib"
-      "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/haloscopeSim")
+    file(RPATH_CHECK
+         FILE "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/haloscopeSim"
+         RPATH "")
+  endif()
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "/home/po524/Documents/haloscope/V2/build/haloscopeSim")
+  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/haloscopeSim" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/haloscopeSim")
+    file(RPATH_CHANGE
+         FILE "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/haloscopeSim"
+         OLD_RPATH "/opt/geant4/geant4-10.4/lib:/opt/anaconda3/lib:/usr/local/lib:"
+         NEW_RPATH "")
     if(CMAKE_INSTALL_DO_STRIP)
-      execute_process(COMMAND "/Library/Developer/CommandLineTools/usr/bin/strip" -u -r "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/haloscopeSim")
+      execute_process(COMMAND "/usr/bin/strip" "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/haloscopeSim")
     endif()
   endif()
 endif()
@@ -53,5 +65,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/panos/Documents/NYU/6.Extracurricular/11.Haloscope/0.Simulations/V2/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/po524/Documents/haloscope/V2/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
